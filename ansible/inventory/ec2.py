b47b82a3c1c050ff0cda651a7d4b1221d3617c73
Jonathan Gallimore <jon@jrg.me.uk>
2022-10-21 09:30:11 +0100

    Use latest Ansible and Bato. Update ActiveMQ to 5.17.5

--- ansible/inventory/ec2.py
+++ ansible/inventory/ec2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# Copyright: (c) 2012, Peter Sankauskas
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 '''
 EC2 external inventory script
@@ -135,53 +140,35 @@ These settings would produce a destination_format as the following:
 'webserver-ansible-blue-172.31.0.1'
 '''
 
-# (c) 2012, Peter Sankauskas
-#
-# This file is part of Ansible,
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-
-######################################################################
-
-import sys
-import os
 import argparse
+import json
+import os
 import re
-from time import time
+import sys
+from collections import defaultdict
 from copy import deepcopy
 from datetime import date, datetime
+from time import time
+
 import boto
 from boto import ec2
-from boto import rds
 from boto import elasticache
+from boto import rds
 from boto import route53
 from boto import sts
 
-from ansible.module_utils import six
-from ansible.module_utils import ec2 as ec2_utils
-from ansible.module_utils.six.moves import configparser
-
 HAS_BOTO3 = False
 try:
-    import boto3  # noqa
+    # Used so that we can cleanly fail, some of our (optional) dependencies need this
+    import boto3  # pylint: disable=unused-import
     HAS_BOTO3 = True
 except ImportError:
     pass
 
-from collections import defaultdict
+from ansible.module_utils import six
+from ansible.module_utils.six.moves import configparser
+from ansible_collections.amazon.aws.plugins.module_utils import ec2 as ec2_utils
 
-import json
 
 DEFAULTS = {
     'all_elasticache_clusters': 'False',
@@ -326,7 +313,7 @@ class Ec2Inventory(object):
         if six.PY3:
             config = configparser.ConfigParser(DEFAULTS)
         else:
-            config = configparser.SafeConfigParser(DEFAULTS)
+            config = configparser.SafeConfigParser(DEFAULTS)  # pylint: disable=deprecated-class
         ec2_ini_path = os.environ.get('EC2_INI_PATH', defaults['ec2']['ini_path'])
         ec2_ini_path = os.path.expanduser(os.path.expandvars(ec2_ini_path))
 
